842555451e1f7d7802d86a79c7dbcc295c5c21df
Ahmed Ayan <i250585@isb.nu.edu.pk>
2026-07-17 23:28:41 +0500

    added muhaqqiq in the init

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,15 @@ from .darban import classify
 from .mehakkim import validate
 from .ustad import stream_answer
 from .imtehaan import start_quiz, evaluate_answer
+from .muhaqqiq import analyze_paper, cross_examine_claim, synthesize_multiple_papers
 
-__all__ = ["classify", "validate", "stream_answer", "start_quiz", "evaluate_answer"]
+__all__ = [
+    "classify",
+    "validate",
+    "stream_answer",
+    "start_quiz",
+    "evaluate_answer",
+    "analyze_paper",
+    "cross_examine_claim",
+    "synthesize_multiple_papers"
+]
